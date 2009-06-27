1b0a3c80c436699d677fc7b70320e002987bb03a
Edson/Home <edonle@gmail.com>
2009-06-27 18:07:41 -0300

    Adicionando Scaffold Category

--- db/migrate/20090626202426_add_default_user.rb
+++ db/migrate/20090626202426_add_default_user.rb
@@ -1,5 +1,5 @@
 class AddDefaultUser < ActiveRecord::Migration
-self.up
+def self.up
   if !User.find_by_login('admin')
     User.create(:login => 'admin', :email => 'edonle@gmail.com', 
                 :password => 'changeme', :password_confirmation => 'changeme')
