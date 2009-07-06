a80e0dde9c6111e6b1ef9baf3ff623f95149c0c3
Edson/Work <edonle@gmail.com>
2009-07-06 18:25:16 -0300

    Resolvendo problemas de layout no index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@
 class ApplicationController < ActionController::Base
 
   include AuthenticatedSystem
-	
+  
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
